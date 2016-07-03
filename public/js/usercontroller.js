924cdc68bcf0a7481300fa6fbb8ad5c07f99c373
Eileen <ejzhong@berkeley.edu>
2016-07-02 23:21:27 -0700

    capitalize 1st ltr

--- public/js/usercontroller.js
+++ public/js/usercontroller.js
@@ -24,5 +24,11 @@ angular.module('myApp')
   }
 
 
-});
+})
 
+
+.filter('capitalize', function() {
+    return function(input) {
+      return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+    }
+});
